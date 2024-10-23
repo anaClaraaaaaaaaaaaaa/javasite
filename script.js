aaeab6f0e9f4fab4a5e64ef578767cd5b8e93367
anaClaraaaaaaaaaaaaa <22301593@aluno.cotemig.com.br>
2024-10-23 14:43:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('botao').addEventListener('click', function() 
+
+{ document.body.classList.toggle('dark-mode'); document.body.classList.toggle('light-mode'); });
+
+window.addEventListener('load', function() { document.body.classList.add('loaded'); });
